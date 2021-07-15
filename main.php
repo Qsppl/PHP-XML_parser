a4b704cf1d9f731858644ff28b313ba735ac2c8a
Tomoe <4.artem.0@gmail.com>
2021-07-15 18:39:32 +0300

    Update main.php

--- main.php
+++ main.php
@@ -15,9 +15,10 @@
 // чтение файла из потока и обработку файла сегментами скорее всего не нужно реализовывать для тестового задания
 // так же в тз не указано что нужно реализовать методы записи данных обратно в xml
 
-class XML // каждый экземпляр класса представляет собой один XmlElement, 
+class XML implements Countable, ArrayAccess, IteratorAggregate // каждый экземпляр класса представляет собой один XmlElement, 
 {
-    // СВОЙСТВА XML ЭЛЕМЕНТА
+    // СВОЙСТВА XML ЭЛЕМЕНТА //
+
     private $pointer; // используется для построения dom во время парсинга
 
     private $tagName;
@@ -33,6 +34,8 @@ class XML // каждый экземпляр класса представляе
     private $iterPos;
 
 
+    // конструктор он и есть конструктор //
+
     public function __construct($data){
         if( !((is_string($data)) or (is_array($data))) ){throw new Exception('TypeError');}; // перегрузки нет, объявления типов аргументов тоже. пишу костыль.
 
@@ -44,16 +47,78 @@ class XML // каждый экземпляр класса представляе
         }
     }
 
+
+    // простые методы класса //
+
     public function appendChild(self $element){
         $this -> childs[$element->tagName][] = &$element;
         $element->parent = &$this;
         return true;
     }
 
-    // интерфейсы
-    
 
-    // парсер
+    // перегрузка вызова private/несуществующих свойств  //
+
+    public function __get(string $tagName){ // получать элементы будем через оператор '->' <<< $document->$element (p.s. я слишком поздно понял что так нельзя делать, это может заблокировать доступ к приватным свойствам)
+        if(!(isset($this->childs[$tagName]))) {throw new Exception("\"{$tagName}\" Element not found");}
+
+        if (count($this->childs[$tagName]) == 1) { 
+            if ($this->childs[$tagName][0] instanceof self) {
+                return $this->childs[$tagName][0];  // если потомок с таким $tagName всего один- вернуть его
+            } else {throw new Exception('typeError');}
+
+        } else {
+            if (is_array($this->childs[$tagName])){
+                return $this->childs[$tagName]; // иначе вернуть весь массив с потомками
+            } else {throw new Exception('typeError');}
+
+        }
+    }
+
+
+    // служебный метод для получения объекта как строки. через него будем получать data //
+
+    public function __toString():string { // если мы обращаемся к элементу как к строке, он возвращает data <<< echo $element
+        return $this-> cdata;
+    }
+
+
+    // интерфейсы //
+    
+    //ArrayAccess - если мы обращаемся к элементу как к массиву, он возвращает атрибуты
+    public function offsetExists($offset){ return isset($this->attributes[$offset]); } // [666] Данный метод выполняется при использовании isset() или empty() на объектах, реализующих интерфейс ArrayAccess.
+    public function offsetGet($offset){
+        if(!(isset($this[$offset])) ){ // [666] такие условия писать не стоит
+            throw new Exception("\"{$offset}\" Attribute not found");
+        }
+        return $this->attributes[$offset];
+    }
+    public function offsetSet($offset, $value): void {
+        $this->attributes[$offset] = $value;
+    }
+    public function offsetUnset($offset): void {
+        unset($this->attributes[$offset]);
+    }
+    
+    // Countable - __get() перегружен, возвращает array или XMLObject. 
+        // если попробовать посчитать XMLObject, получится что-то не то, так что перегружаем count()
+    public function count(): int {
+        if ($this->parent == null) { return 1; } // если мы попытались посчитать корневой элемент, возвращаем 1
+        
+        return count($this->parent->childs[$this->tagName]);
+    }
+
+    //IteratorAggregate - при обращении к объекту как к иттерируемому <<< foreach $document->$ul->li
+        // [010] мы хотим итерировать не сам объект, а всех потомков родителя этого объекта. __get() перегружен, возвращает array или XMLObject
+    public function getIterator(): array {
+        if ($this->parent == null) { return array($this); } // если мы попытались итерировать корневой элемент, возвращаем массив с ним
+        
+        return $this->parent->childs[$this->tagName]; // [010]
+    }
+
+
+    // парсер //
+
     private function parse(string $data){
         // настройки парсера
         $parser = xml_parser_create();
@@ -84,8 +149,5 @@ class XML // каждый экземпляр класса представляе
 }
 
 $document = file_get_contents('source/testfile.xml');
-$xmlstructure = new XML("<ЗначенияСвойства>
-<Ид>40a3098f-ed8f-11e8-af65-c6a3bfc032f2</Ид>
-<Значение>229f20f6-543e-11e9-8458-485b3977ac2a</Значение>
-</ЗначенияСвойства>");
+$xmlstructure = new XML($document);
 var_dump($xmlstructure);
