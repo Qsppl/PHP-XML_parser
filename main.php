e19e9fe7e4c401eda9bc941c959172dafda1b4ca
Tomoe <4.artem.0@gmail.com>
2021-08-01 00:11:30 +0300

    Update main.php

--- main.php
+++ main.php
@@ -2,27 +2,19 @@
 
 require 'src/autoload.php';
 
+use src\Product;
 use src\XmlElement;
 
 $document = file_get_contents('Data/import___bc236459-b687-474a-8555-427a5c7d44da.xml');
-
 $xmlstructure = new XmlElement($document);
-
-print($xmlstructure);
-
-print("\n");
-
-print($xmlstructure->КоммерческаяИнформация->Каталог['СодержитТолькоИзменения']);
-
-print("\n");
+unset($document);
 
 $catalog = $xmlstructure->КоммерческаяИнформация->Каталог;
+$products = array();
+foreach ($catalog->Товары->Товар as $product) {
+    $products[] = new Product($product);
+}
 
-if ($catalog['СодержитТолькоИзменения'] == true) {
-    foreach ($catalog->Товары->Товар as $product) {
-        print($product->Наименование);
-
-        print("\n");
-    }
-
+foreach ($products as $product) {
+    print($product->name."\n");
 }
